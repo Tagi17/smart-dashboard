7dbf5cfcb472fc57d59f4fa4df0f44dbee31f70a
Nazia <naziaozair@gmail.com>
2023-06-02 17:17:31 -0400

    reorganized code

---
+++ services/uniswap.tsx
@@ -0,0 +1,40 @@
+import { ChainId } from '@uniswap/sdk/dist/constants';
+import { Fetcher } from '@uniswap/sdk/dist/fetcher';
+import { Token } from '@uniswap/sdk';
+import { ethers } from 'ethers';
+import { useEffect } from 'react';
+
+const provider = new ethers.JsonRpcProvider("htpps://polygon-mumbai.infura.io/v3/952063985f82462c88e42f4ed150b486");
+
+const chainId: ChainId = ChainId.MATIC_MUMBAI;
+
+export async function fetchTokenData(tokenAddress: string) {
+  const network = await provider.getNetwork();
+  const chainId = network.chainId; // set the chain 
+  const token = new Token(chainId, tokenAddress, 18, 'TOKEN SYMBOL', 'TOKEN NAME');
+  const tokenWithInfo = await Fetcher.fetchTokenData(chainId, tokenAddress);
+
+  const { name, symbol, decimals } = tokenWithInfo;
+
+  return {
+    address: tokenAddress,
+    name,
+    symbol,
+    decimals,
+  };
+}
+
+function ShowData() {
+  useEffect(() => {
+    async function fetchData() {
+      const tokenAddress = '0x2791bca1f2de4661ed88a30c99a7a9449aa84174';
+      const tokenData = await fetchTokenData(tokenAddress);
+      console.log(tokenData);
+    }
+    fetchData();
+  }, []);
+
+  return <div>Data</div>;
+}
+
+export default ShowData;
