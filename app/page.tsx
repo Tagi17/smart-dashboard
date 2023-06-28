6862f9244de04f990f17de4bab48c445d069e2cf
Nazia <naziaozair@gmail.com>
2023-06-28 18:15:53 -0400

    fixed border radius

--- app/page.tsx
+++ app/page.tsx
@@ -12,19 +12,24 @@ export default async function Page() {
         <div className="uniswap">
           Gimmie yo money  
         </div>
-        <div className="flex">
-          <div className="flex-auto w-1 mr-10 ml-5">
-              <div className="curved-box">
-                <div className="uniswap">user wallet info</div>
-              </div>
-          </div>
-            <div className="flex-auto w-96 mr-5">
-              <div className="curved-box">
+          <div className="flex">
+            <div className="flex-auto w-1 mr-10 ml-5">
+                <div className="curved-box">
+                  <div className="uniswap">user wallet info</div>
+                </div>
+            </div>
+            <div className="flex-auto w-10 rounded-l-lg">
+              <div className="curved-box1">
                 <div className="uniswap">UNISWAP</div>
                 <div className="uniswap">See all your staked crypto</div>
               </div>
-          </div>
-        </div>     
+            </div>
+            <div className="flex-auto w-96 mr-5 rounded-r-lg">
+              <div className="curved-box2">
+                <div className="uniswap">UNISWAP</div>
+              </div>
+            </div>
+          </div>     
     </div>
   );
 }
